bc05d6cc263c72f02e6feb515e44a2c8f066f814
Matt Carroll <matt.carroll@optimizely.com>
2019-07-08 16:22:15 -0700

    Remove redux example

--- packages/example-react-redux/src/OptimizelyReduxProvider.js
+++
@@ -1,8 +0,0 @@
-import { OptimizelyProvider } from '@optimizely/react-sdk'
-import { connect } from 'react-redux'
-
-const mapStateToProps = state => ({
-  user: state.user,
-})
-
-export default connect(mapStateToProps)(OptimizelyProvider)
--- packages/example-react-redux/src/modules/actions.js
+++
@@ -1,9 +0,0 @@
-// types of action
-export const Types = {
-  SET_USER: "SET_USER",
-};
-
-export const setUser = (user) => ({
-  types: Types.SET_USER,
-  payload: user
-})
